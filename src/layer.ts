6e02c893f60bbd6f96f3bc9c0492b1515f6fbb3d
Max Bickers <mbickers@fastmail.com>
2026-08-16 11:07:42 -0400

    combine layers

--- src/layer.ts
+++ src/layer.ts
@@ -3,8 +3,7 @@ export const SERVICE_PERIODS = ["regular", "late_night", "weekend"] as const;
 export type ServicePeriod = (typeof SERVICE_PERIODS)[number];
 
 export type Layer =
-  | { kind: "geography" }
+  | { kind: "geography"; parksVisible: boolean }
   | { kind: "streets" }
-  | { kind: "parks" }
   | { kind: "bike_lanes" }
   | { kind: "subway"; servicePeriod: ServicePeriod };
