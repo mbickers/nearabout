ac433d66f97304372d96f73434dad87729240387
Max Bickers <mbickers@fastmail.com>
2026-08-27 07:58:24 -0400

    Refresh stale worktree data files during bootstrap

--- data/bootstrap_worktree.sh
+++ data/bootstrap_worktree.sh
@@ -4,7 +4,7 @@ set -eu
 primary_checkout=$(dirname "$(git rev-parse --path-format=absolute --git-common-dir)")
 worktree_root=$(git rev-parse --show-toplevel)
 if [ "$worktree_root" != "$primary_checkout" ]; then
-    cp -cRn "$primary_checkout/public/data" public/
+    rsync -a "$primary_checkout/public/data/" public/data/
 fi
 uv sync
 npm install
